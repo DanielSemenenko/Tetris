b894af1e3563352ccbb15aeb54d201407b0da9e4
DanielSemenenko <75879067+DanielSemenenko@users.noreply.github.com>
2021-06-22 21:44:21 +0200

    Add two points on screen

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,27 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowPosition(40, 30);
+            Console.SetBufferSize(40, 30);
+            int x1 = 3;
+            int y1 = 4;
+            char c1 = '*';
+            int y2 = 5;
+            int y3 = 6;
+
+            Draw(x1, y1, c1);
+            Draw(x1, y2, c1);
+            Draw(x1, y3, c1);
+
+
+            Console.ReadLine();
+
+        }
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+
+            Console.Write(c);
         }
     }
 }
